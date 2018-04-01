aafa1fe91096d567db1087c1331afc368911c005
Rani Shah <rani.shah@flatironschool.com>
2018-04-01 13:28:57 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+ get '/name' do
+  "My name is Rani"
+end
+
+get '/hometown' do
+  "My hometown is London!"
+end
+
+get '/favorite-song' do
+  "My favorite song is Wonderwall"
+end
+
+
 end
