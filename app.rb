d5134cc1200b77ef10e0a8a4b8861616175747de
victoria-huang <vctrah@gmail.com>
2018-04-01 15:36:48 -0400

    add 3 get routes

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Vicky"
+  end
+
+  get '/hometown' do
+    "My hometown is East Brunswick, NJ"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Blue Christmas"
+  end
 end
