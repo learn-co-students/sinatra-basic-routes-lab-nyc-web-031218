1c33d1a737d4e80f980544d92e8dcf07c703507d
harim1206 <harim1206@gmail.com>
2018-04-02 10:43:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Harim"
+
+  end
+
+  get '/hometown' do
+    "My hometown is Seoul"
+
+  end
+
+  get '/favorite-song' do
+    "My favorite song is erotic city"
+
+  end
 end
